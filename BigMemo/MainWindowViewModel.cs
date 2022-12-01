e9b18c41676dd78a1a3599f8a22e10583abe7323
daichi takada <03daichi28@gmail.com>
2022-12-02 00:07:18 +0900

    component色々配置

---
+++ BigMemo/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BigMemo
+{
+	public class MainWindowViewModel
+	{
+		public double Width { get; set; }
+		public double Height { get; set; }
+		public double Top { get; set; }
+		public double Left { get; set; }
+
+		public MainWindowViewModel()
+		{
+			Width = 1600;
+			Height = SystemParameters.WorkArea.Height + 10;
+			Top = 0;
+			Left = SystemParameters.WorkArea.Width - Width + 5;
+		}
+	}
+}
